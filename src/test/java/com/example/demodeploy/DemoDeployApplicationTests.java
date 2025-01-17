2b2f3e3e9f11d2062b6115b16c2b2240baf2dfc1
seojin2000 <wesms0620@gmail.com>
2025-01-17 15:51:27 +0900

    단위 테스트 커밋 해결

--- src/test/java/com/example/demodeploy/DemoDeployApplicationTests.java
+++ src/test/java/com/example/demodeploy/DemoDeployApplicationTests.java
@@ -10,10 +10,11 @@ class DemoDeployApplicationTests {
     void contextLoads() {
         // 단위 테스트
         // MVC 테스트 -> 목업통해서 가상 처리 가능
+        // 테스트 실패되면 반영 X (깃에는 반영, 배포및 운영에 반영 X)
         // RestAPI 테스트 가능
         // 메세지가 보이진 않음
-        int a = 1/0;
-        throw new RuntimeException("오류발생");
+        //int a = 1/0;
+        //throw new RuntimeException("오류발생");
         //System.out.println("단위 텍스트 호출||echo");
     }
 
