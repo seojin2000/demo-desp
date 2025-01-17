ff36947d8452df266bf98d13ac507d41330809a2
seojin2000 <wesms0620@gmail.com>
2025-01-17 11:46:54 +0900

    EC2업로드

--- src/test/java/com/example/demodeploy/DemoDeployApplicationTests.java
+++ src/test/java/com/example/demodeploy/DemoDeployApplicationTests.java
@@ -11,6 +11,7 @@ class DemoDeployApplicationTests {
         // 단위 테스트
         // MVC 테스트 -> 목업통해서 가상 처리 가능
         // RestAPI 테스트 가능
+        // 메세지가 보이진 않음
         System.out.println("단위 텍스트 호출||echo");
     }
 
