b871a21e02909be9d0829e0309de82f83de1ceef
seojin2000 <wesms0620@gmail.com>
2025-01-17 14:44:12 +0900

    오류 테스트

--- src/test/java/com/example/demodeploy/DemoDeployApplicationTests.java
+++ src/test/java/com/example/demodeploy/DemoDeployApplicationTests.java
@@ -12,7 +12,9 @@ class DemoDeployApplicationTests {
         // MVC 테스트 -> 목업통해서 가상 처리 가능
         // RestAPI 테스트 가능
         // 메세지가 보이진 않음
-        System.out.println("단위 텍스트 호출||echo");
+        int a = 1/0;
+        throw new RuntimeException("오류발생");
+        //System.out.println("단위 텍스트 호출||echo");
     }
 
 }
